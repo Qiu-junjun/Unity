538ae67b28f889369e0e157d9548c2fc6643b588
路过的风 <73782469+Qiu-junjun@users.noreply.github.com>
2021-05-11 18:46:08 +0800

    Add files via upload

---
+++ loadPrefab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class loadPrefab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject tip;
+    private GameObject Tip;
+    private GameObject talk;
+    private GameObject talk1;
+
+    public Button send;
+    public Canvas Canvas;
+    void Start()
+    {
+        send.onClick.AddListener(loadTalking);
+    }
+    private void loadPrefabs(GameObject prefab,GameObject gameObject)
+    {
+        gameObject = Instantiate(prefab);
+        gameObject.transform.SetParent(Canvas.transform, false);
+    }//用来动态加载一个预制体，并将其设置为画布的子物体
+    void loadTalking()
+    {
+        loadPrefabs(tip, Tip);
+    }
+}
---
+++ test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public GameObject prefab;//用来接收一个prefab
+    public bool isCanCreat
+    {
+        get
+        {
+            return time <= 0f;
+        }
+    }//用来判断time变量是否已经为0或负数，即时间是否达到两秒
+    private float time;//时间间隔
+    void Start()
+    {
+        time = 2f;//时间间隔设置为2秒
+    }
+    void creatPrefab()
+    {
+        Instantiate(prefab);//实例化一个prefab
+        time = 2f;//使time变量归位
+        
+    }
+    void Update()
+    {
+        if(time > 0)
+        {
+            time -= Time.deltaTime;//减去每一帧所消耗的时间
+        }
+        if(isCanCreat)
+        {
+            creatPrefab();
+        }
+    }
+}
